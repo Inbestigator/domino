add96ae88ef678322818aa05529ac1913c7b35b5
Inbestigator <inbestigatoryt@gmail.com>
2025-10-05 03:00:46 -0700

    fix(bot): no dir import

--- www/api/bot.ts
+++ www/api/bot.ts
@@ -1,5 +1,5 @@
 import { handleRequest } from "dressed/server";
 // @ts-ignore
-import { commands, components, events, config } from "../.dressed";
+import { commands, components, events, config } from "../.dressed/index.js";
 
 export const POST = (req: Request) => handleRequest(req, commands, components, events, config);
